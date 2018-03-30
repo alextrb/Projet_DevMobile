d2dc1d80f2f6a04e622c742335b557f207dd6832
Alex <trbovic.alexandre@hotmail.fr>
2018-03-30 16:20:28 +0200

    AddMatchActivity lorsqu'on clic sur le bouton + de AllMatchesActivity

--- app/src/main/java/app/univers7/ultra_instinct/AllMatchesActivity.java
+++ app/src/main/java/app/univers7/ultra_instinct/AllMatchesActivity.java
@@ -45,12 +45,13 @@ public class AllMatchesActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_match_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(AllMatchesActivity.this, AddMatchActivity.class);
             }
         });
 
