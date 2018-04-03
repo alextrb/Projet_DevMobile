0c0099dc2e510498dad0ca09f454286a72e8db26
Ong Philippe <ong.philippe@gmail.com>
2018-04-03 22:57:23 +0200

    Geocoding pas terminé

--- app/src/main/java/app/univers7/ultra_instinct/AllMatchesDetailsActivity.java
+++ app/src/main/java/app/univers7/ultra_instinct/AllMatchesDetailsActivity.java
@@ -1,6 +1,8 @@
 package app.univers7.ultra_instinct;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,6 +22,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class AllMatchesDetailsActivity extends AppCompatActivity
@@ -99,6 +105,8 @@ public class AllMatchesDetailsActivity extends AppCompatActivity
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        getGeocoding(latitude, longitude);
+
     }
 
     @Override
@@ -171,5 +179,32 @@ public class AllMatchesDetailsActivity extends AppCompatActivity
                 .title("Marker in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(match_LatLng, 11));
 
+    }
+
+    public void getGeocoding(Double latitude, Double longitude)
+    {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName();
+
+            System.out.println(address);
+            System.out.println(city);
+            System.out.println(state);
+            System.out.println(country);
+            System.out.println(postalCode);
+            System.out.println(knownName);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
